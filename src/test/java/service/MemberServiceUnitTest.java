20bf36e436d628a0f27a7a5e62dd08e5abfb7c53
itsonyeji <iqkeki@gmail.com>
2023-06-29 15:17:43 +0900

    MemberService loginMember 단위 테스트

--- src/test/java/service/MemberServiceUnitTest.java
+++ src/test/java/service/MemberServiceUnitTest.java
@@ -28,4 +28,13 @@ public class MemberServiceUnitTest {
         /* assertEquals(테스트메서드, 검증값) */
         assertEquals(msrv.saveMember(m),true);
     }
+
+    @Test
+    public void loginMember() throws Exception{
+        Member m=new Member();
+        m.setUserid("abc123");
+        m.setPasswd("987xyz");
+        /* assertEquals(테스트메서드, 검증값) */
+        assertEquals(msrv.loginMember(m),true);
+    }
 }
