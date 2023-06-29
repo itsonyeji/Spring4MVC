31f80d7ff22b2bf351d8bb1349b507c89b7783b2
itsonyeji <iqkeki@gmail.com>
2023-06-29 15:17:18 +0900

    MemberService loginMember 구현

--- src/main/java/ityeji/hello/spring4/service/MemberService.java
+++ src/main/java/ityeji/hello/spring4/service/MemberService.java
@@ -4,4 +4,5 @@ import ityeji.hello.spring4.model.Member;
 
 public interface MemberService {
     boolean saveMember(Member m);
+    boolean loginMember(Member m);
 }
--- src/main/java/ityeji/hello/spring4/service/MemberServiceImpl.java
+++ src/main/java/ityeji/hello/spring4/service/MemberServiceImpl.java
@@ -19,4 +19,12 @@ public class MemberServiceImpl implements MemberService{
 
         return isSaved;
     }
+
+    @Override
+    public boolean loginMember(Member m) {
+        boolean isLogin=false;
+
+        if(mdao.loginMember(m) != null) isLogin=true;
+        return isLogin;
+    }
 }
