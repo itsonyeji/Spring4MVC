71e66ddc7dc352b58945c8273e8e94f772a287ee
itsonyeji <iqkeki@gmail.com>
2023-06-30 16:53:28 +0900

    BoardService : readBoard 구현

---
+++ src/main/java/ityeji/hello/spring4/service/BoardService.java
@@ -0,0 +1,9 @@
+package ityeji.hello.spring4.service;
+
+import ityeji.hello.spring4.model.Board;
+
+import java.util.List;
+
+public interface BoardService {
+    List<Board> readBoard(int cpg);
+}
