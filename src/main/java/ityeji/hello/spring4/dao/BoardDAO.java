4c35481ef2dbc3009ebf4837a62029fd95c3731b
itsonyeji <iqkeki@gmail.com>
2023-07-03 14:51:51 +0900

    BoardDAO : 조회수 증가, insertBoard 구현

--- src/main/java/ityeji/hello/spring4/dao/BoardDAO.java
+++ src/main/java/ityeji/hello/spring4/dao/BoardDAO.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface BoardDAO {
     List<Board> selectBoard(int snum);
     Board selectOneBoard(String bno);
+
+    int insertBoard(Board bd);      /* 함수를 쓰는 장점, 여러개를 넘길수 있고 캡슐화 가능 */
 }
